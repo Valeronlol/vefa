13eff1282f91104d11d8e1a1336591f370183976
Valeron <green-travel.kg@mail.ru>
2017-01-03 19:33:46 +0600

    header ready

--- wp-content/themes/valeron/frontend/app/js/common.js
+++ wp-content/themes/valeron/frontend/app/js/common.js
@@ -1,5 +1,9 @@
 $(function() {
 
-	// Custom JS
+	$('header .header-slick').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1
+    });
 
 });
--- wp-content/themes/valeron/frontend/gulpfile.js
+++ wp-content/themes/valeron/frontend/gulpfile.js
@@ -18,7 +18,8 @@ var gulp           = require('gulp'),
 gulp.task('scripts', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
-		'app/js/common.js', // Всегда в конце
+		'app/libs/slick-carousel/slick/slick.min.js',
+		'app/js/common.js' // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
 	.pipe(uglify())
