a833d034f3d3b859bc9cdcefa421ea42d9033c32
Valeron <green-travel.kg@mail.ru>
2017-01-04 12:45:14 +0600

    main page ready

--- wp-content/themes/valeron/frontend/app/js/common.js
+++ wp-content/themes/valeron/frontend/app/js/common.js
@@ -1,9 +1,19 @@
 $(function() {
 
+    /**
+     * Header slider
+     */
 	$('header .header-slick').slick({
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1
+    });
+
+    $('.main-page-item').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true
     });
 
 });
