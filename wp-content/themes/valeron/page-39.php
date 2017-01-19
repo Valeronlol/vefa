b02739f58c982f6c8eac39e7da564aae08ac02e2
Valeron <green-travel.kg@mail.ru>
2017-01-19 22:31:33 +0600

    pagination

--- wp-content/themes/valeron/page-39.php
+++ wp-content/themes/valeron/page-39.php
@@ -3,16 +3,20 @@
  * Shop page
  */
 $thumb = get_the_post_thumbnail_url() ? get_the_post_thumbnail_url() : '../wp-content/themes/valeron/frontend/dist/img/default_page_preview.jpg';
-$posts = get_posts(array(
-        'category' => 7,
+$paged = get_query_var( 'paged' ) ? absint( get_query_var( 'paged' ) ) : 1;
+$the_query = new WP_Query( array(
+	    'post_type' => 'post',
+	    'posts_per_page' => 0,
+        'cat' => 7,
         'numberposts' => 0,
         'orderby' => 'rand',
-        'order'    => 'ASC'
+        'order'    => 'ASC',
+	    'paged' => $paged
 ));
 ?>
 <?php get_header();?>
 
-<?php if (have_posts()) : ?>
+<?php if ($the_query->have_posts()) : ?>
 
     <div class="page-preview" style="background-image: url('<?php echo $thumb; ?>')">
         <div class="container">
@@ -31,27 +35,41 @@ $posts = get_posts(array(
         </div>
     </div>
 
-	<?php while (have_posts()) : the_post(); ?>
-        <div class="shop-page-content">
-            <div class="container">
-                <div class="row">
-
-                    <?php foreach ( $posts as $post): ?>
-                        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 cont">
-                            <div class="img" style="background-image: url('<?php echo get_the_post_thumbnail_url($post->ID) ;?>')"></div>
-                            <a href="<?php echo get_permalink($post->ID) ;?>"><h2><?php the_title() ;?></h2></a>
-                        </div>
-                    <?php endforeach;?>
-                </div>
+    <div class="shop-page-content">
+        <div class="container">
+            <div class="row">
+                <?php while ($the_query-> have_posts()) : $the_query->the_post(); ?>
+                    <?php
+                        global $post;
+                        $thumbnail = get_the_post_thumbnail_url($post->ID);
+                        if ( !$thumbnail ) $thumbnail = '../wp-content/themes/valeron/frontend/dist/img/default_page_item.jpg';
+                    ?>
+                    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 cont">
+                        <div class="img" style="background-image: url('<?php echo $thumbnail ;?>')"></div>
+                        <a href="<?php echo get_permalink($post->ID) ;?>"><h2><?php the_title() ;?></h2></a>
+                    </div>
+                <?php endwhile; ?>
             </div>
         </div>
-	<?php endwhile; ?>
+    </div>
+    <?php wp_reset_postdata();?>
+
+
 
     <div class="container shop-more">
         <div class="row">
-            <div id="shop-more">развернуть</div>
+            <div id="pagi">
+	            <?php echo paginate_links(array(
+		            'base'    => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
+		            'format'  => '?paged=%#%',
+		            'current' => max( 1, get_query_var('paged') ),
+		            'total'   => $the_query->max_num_pages
+	            ));?>
+            </div>
         </div>
     </div>
+
+
 
 <?php else : ?>
     <div class="container">
